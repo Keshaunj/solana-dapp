42e8b1a44b0f269610ee9f86d28e42ebf57d6334
Keshaunj <keshaunjones48@gmail.com>
2025-01-31 01:07:45 -0500

    added auth login & jwt

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import User from "./models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 dotenv.config();
 
 const app = express();
@@ -52,6 +53,56 @@ app.post("/signup", async (req, res) => {
     res.status(500).json({ message: "Error creating user" });
   }
 });
+
+const authenticateToken = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access denied, no token provided" });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified; 
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
+
+app.post('/login' , async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (!existingUser) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // my jwt generator
+    const token = jwt.sign(
+      { userId: existingUser._id, username: existingUser.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "3min" } // token login time window/ session vancant time
+    );
+
+    res.status(200).json({ message: "Login successful", token });
+
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Error logging in" });
+  }
+});
+
 
 const PORT = process.env.PORT || 3000;
 
